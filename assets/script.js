c2deca865741dfa684285a51596dca7de3dca9a0
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-06-30 14:40:56 -0700

    java correction commit

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,32 @@ function generate() {
     var numbers = "1234567890";
     var punctuation = "!@#$%^&*()"
 
-    var lowercaseinput= document.getElementById
+    var lowercaseInput= document.getElementById("lowercase");
+    var uppercaseInput = document.getElementById("uppercase");
+    var punctuationInput = doucment.getElementById("punctuation");
+    var numbersInput = document.getElementById("numbers");
+    var passlength = doucment.getElementById("test").value;
 
+    var userpassword = "";
+    var passwordCharSet = "";
+
+    if (lowercaseInput.checked) {
+        passwordCharSet += lowercase;
+    }
+    if (uppercaseInput.checked) {
+        passwordCharSet += uppercase;
+    }
+    if (punctuationInput.checked) {
+        passwordCharSet += punctuation;
+    }
+    if (numbersInput.checked) {
+        passwordCharSet += numbers;
+    }
+    for (var i = 0; i < passlength; i++) {
+    userpassword += passwordCharSet.charAt(Math.floor(Math.random)() * passwordCharSet.length    
+    }
+    document.getElementById("display").value= userpassword;
+    document.getElementById("length").innerHTML = ("Length(8-128): ");
+    document.getElementById("text").oninput = function(){
+        if (document.getElementById("text").value >7){
 }
