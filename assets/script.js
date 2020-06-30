66a590eb307c4fd27f6d41a456d71a38ab69abbf
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-06-29 22:52:02 -0700

    initial js script commit

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+function generate() {
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "1234567890";
+    var punctuation = "!@#$%^&*()"
+
+    var lowercaseinput= document.getElementById
+
+}
