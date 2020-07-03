011f386f35f1fb2f6b64aea7bb7fe05d675d348e
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-07-02 21:53:53 -0700

    index commit

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,20 @@
 
-function generate() {
+function generate(){
+
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbers = "1234567890";
     var punctuation = "!@#$%^&*()"
 
     var lowercaseInput= document.getElementById("lowercase");
-    var uppercaseInput = document.getElementById("uppercase");
-    var punctuationInput = doucment.getElementById("punctuation");
-    var numbersInput = document.getElementById("numbers");
-    var passlength = doucment.getElementById("test").value;
+   
+    var uppercaseInput= document.getElementById("uppercase");
+    var punctuationInput= doucment.getElementById("punctuation");
+    var numbersInput= document.getElementById("numbers");
+    var passlength= doucment.getElementById("text").value;
 
-    var userpassword = "";
-    var passwordCharSet = "";
+    var userpassword= "";
+    var passwordCharSet= "";
 
     if (lowercaseInput.checked) {
         passwordCharSet += lowercase;
@@ -26,11 +28,23 @@ function generate() {
     if (numbersInput.checked) {
         passwordCharSet += numbers;
     }
+    
     for (var i = 0; i < passlength; i++) {
-    userpassword += passwordCharSet.charAt(Math.floor(Math.random)() * passwordCharSet.length    
+    userpassword += passwordCharSet.charAt(Math.floor(Math.random() * passwordCharSet.length -1));    
     }
+
     document.getElementById("display").value= userpassword;
     document.getElementById("length").innerHTML = ("Length(8-128): ");
     document.getElementById("text").oninput = function(){
         if (document.getElementById("text").value >7){
+            doucment.getElementById("length").innerHTML = "length:" +document.getElementById("text").value;
+}
+else{
+    document.getElementById("length").innerHTML="Length: 8";
+}
+    }
+}
+function copyPassword(){
+    doucment.getElementById("display").select();
+    document.execCommand("copy");
 }
